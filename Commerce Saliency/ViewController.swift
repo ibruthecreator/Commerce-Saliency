b402f6dc9ebd13d2532eab4e4a9bda4427179ad2
Mohammed <mohams3ios01@gmail.com>
2020-05-18 18:04:32 -0400

    Initial

---
+++ Commerce Saliency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commerce Saliency
+//
+//  Created by Mohammed Ibrahim on 2020-05-18.
+//  Copyright © 2020 Mohammed Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Commerce Saliency' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Commerce Salience
+  pod 'Alamofire'
+  
+end
